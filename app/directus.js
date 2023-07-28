21cfa256b33b4bb81e47f48c6478e8ad61fbb52d
bmuday <bmuday@live.fr>
2023-07-28 21:35:12 +0200

    redeploy

--- app/directus.js
+++ app/directus.js
@@ -1,5 +1,5 @@
 import { createDirectus, rest, authentication } from "@directus/sdk";
-export const apiURL = process.env.NEXT_PUBLIC_API_URL as string;
+export const apiURL = process.env.NEXT_PUBLIC_API_URL;
 const client = createDirectus(apiURL).with(rest()).with(authentication("json"));
 
 export default client;
